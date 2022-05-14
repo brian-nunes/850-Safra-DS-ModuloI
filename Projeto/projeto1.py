ad4000c66551e2cebe4e46194f9ffc9172252a4b
Brian Nunes <64751909+brian-nunes@users.noreply.github.com>
2022-05-14 15:31:46 -0300

    Upload Projeto

---
+++ Projeto/projeto1.py
@@ -0,0 +1,86 @@
+import json
+import os.path
+import sys
+
+def obter_dados():
+    '''
+    Essa função carrega os dados dos produtos e retorna uma lista de dicionários, onde cada dicionário representa um produto.
+    NÃO MODIFIQUE essa função.
+    '''
+    with open(os.path.join(sys.path[0], 'dados.json'), 'r') as arq:
+        dados = json.loads(arq.read())
+    return dados
+
+def listar_categorias(dados):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista contendo todas as categorias dos diferentes produtos.
+    Cuidado para não retornar categorias repetidas.    
+    '''
+    ...
+
+def listar_por_categoria(dados, categoria):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar uma lista contendo todos os produtos pertencentes à categoria dada.
+    '''
+    ...
+    
+
+def produto_mais_caro(dados, categoria):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
+    '''
+    ...
+
+
+def produto_mais_barato(dados, categoria):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    O parâmetro "categoria" é uma string contendo o nome de uma categoria.
+    Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
+    '''
+    ...
+
+def top_10_caros(dados):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais caros.
+    '''
+    ...
+
+def top_10_baratos(dados):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais caros.
+    '''
+    ...
+
+def menu(dados):
+    '''
+    O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
+    Essa função deverá, em loop, realizar as seguintes ações:
+    - Exibir as seguintes opções:
+        1. Listar categorias
+        2. Listar produtos de uma categoria
+        3. Produto mais caro por categoria
+        4. Produto mais barato por categoria
+        5. Top 10 produtos mais caros
+        6. Top 10 produtos mais baratos
+        0. Sair
+    - Ler a opção do usuário.
+    - No caso de opção inválida, imprima uma mensagem de erro.
+    - No caso das opções 2, 3 ou 4, pedir para o usuário digitar a categoria desejada.
+    - Chamar a função adequada para tratar o pedido do usuário e salvar seu retorno.
+    - Imprimir o retorno salvo. 
+    O loop encerra quando a opção do usuário for 0.
+    '''
+    ...
+
+
+# Programa Principal - não modificar!
+d = obter_dados()
+menu(d)
